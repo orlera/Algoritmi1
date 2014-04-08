9258eafd2458fef53c23339cfe36366f34368a1f
son93 <michelebof93@gmail.com>
2014-04-08 17:28:36 +0200

    pr

--- generali.cpp
+++ generali.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 void battaglie();
+void disprezza();
 
 
 struct nodo{
   vector<int> figli;
+  vector<int> disprezza;
   bool figlio;
 };
 
@@ -26,6 +29,7 @@ int main(){
     in>>from>>to;
     grafo[from].figli.push_back(to);
   }
+  disprezza();
   battaglie();
   ofstream out("output.txt");
   out<<capi.size()<<"\n";
@@ -39,6 +43,37 @@ int main(){
 
 }
 
+
+
+void disprezza(){
+  for(int i=0;i<N;i++){
+    queue<int> q;
+    for(int j=0;j<grafo[i].figli.size();j++){
+      grafo[i].disprezza.push_back(grafo[i].figli[j]);
+      q.push(grafo[i].figli[j]);     
+      grafo[grafo[i].figli[j]].figlio=true;
+    }
+    while(!q.empty()){
+      int tmp=q.front();
+      q.pop();
+      for(int k=0;k<grafo[tmp].figli.size();k++){
+        int t=grafo[tmp].figli[k];
+        if(!grafo[t].figlio && t!=i){
+          grafo[i].disprezza.push_back(t);
+          q.push(t);
+          grafo[t].figlio=true;
+        }
+      }
+    }
+    for(int k=0;k<N;k++){
+      grafo[k].figlio=false;
+    }
+  }
+
+}
+
+
+
 void battaglie(){
   for(int i=0;i<N;i++){
     for(int j=0;j<grafo[i].figli.size();j++){
@@ -46,6 +81,12 @@ void battaglie(){
       bool val=false;
       for(int k=0;k<ris.size();k++){
         if(ris[k].second==tmp){
+          val=true;
+        }
+      }
+      for(int k=0;k<grafo[tmp].disprezza.size();k++){
+        int t=grafo[tmp].disprezza[k];
+        if(t==i){
           val=true;
         }
       }
